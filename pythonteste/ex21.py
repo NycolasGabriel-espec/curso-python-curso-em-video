da09550278a0c66b0a431ab963345b5891fde7b3
Nycolas Gabriel Santos da Silva <nycolas@MacBook-de-Nycolas.local>
2026-07-12 12:58:04 -0300

    exercício 21

---
+++ pythonteste/ex21.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load("pythonteste/taffyr_music-ciudades-lo-fi-music-produced-by-taffyx27-rhythm-198728.mp3")
+pygame.mixer.music.play()
+
+input("Pressione qualquer tecla para parar a música...")
